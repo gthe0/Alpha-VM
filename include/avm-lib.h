982a6e6ab073d5fcb6d381a43587404a0e02354b
gtheo <giorgostheodoraki@hotmail.com>
2024-06-04 17:12:55 +0300

    made some library functions

---
+++ include/avm-lib.h
@@ -0,0 +1,15 @@
+/*
+ 	Authors: csd4881, csd4988, csd5038														
+	
+	avm-lib.h										
+
+	Interface containing the library functions.
+*/
+
+#ifndef AVM_LIB_H
+#define AVM_LIB_H
+
+
+
+
+#endif /* avm-library */
---
+++ src/avm-lib.c
@@ -0,0 +1,242 @@
+/*
+ 	Authors: csd4881, csd4988, csd5038														
+	
+	avm-lib.c									
+
+	Implementation of the library functions.
+*/
+
+#include <avm-lib.h>
+#include <avm-mem.h>
+#include <avm-log.h>
+#include <avm-utils.h>
+#include <avm-reader.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+/*totalargs function*/
+void libfunc_totalarguments (void)
+{
+	/* Get topsp of prev activation record.
+	*/
+	unsigned p_topsp = avm_get_envvalue(topsp + AVM_SAVEDTOPSP_OFFSET);
+	avm_mem_cell_clear(&retval);
+
+	if(!p_topsp)
+	{
+		avm_log(ERROR,"'totalarguments' called outside a function!\n");
+		retval.type = nil_m ;
+	}
+	else
+	{
+		/*
+		 Extract the number of actual arguments
+		 for the previous activation record.
+		*/
+		retval.type = number_m;
+		retval.data.numVal = avm_get_envvalue(p_topsp + AVM_NUMACTUALS_OFFSET);
+	}
+}
+
+/* Just prints all the arguments 
+by turning them to string first */
+void libfunc_print(void)
+{
+	unsigned n = avm_total_actuals();
+
+	for (unsigned i = 0; i < n; i++)
+	{
+		char* s = avm_tostring(avm_getactual(i));
+		puts(s);
+		free(s);
+	}
+}
+
+/* Retruns the typeof of an argument */
+void libfunc_typeof(void)
+{
+	unsigned n = avm_total_actuals();
+
+	if(n != 1)
+	{
+		avm_log(ERROR,"one argument (not %d) expected in 'typeof'\n",n);
+	}
+	else
+	{
+		avm_mem_cell_clear(&retval);
+		retval.type = string_m;
+		retval.data.strVal = strdup(typeString[avm_getactual(0)->type]);
+	}
+}
+
+/* Returns nth argument */
+void libfunc_argument(void)
+{
+	/* Get topsp of prev activation record.
+	*/
+	unsigned p_topsp = avm_get_envvalue(topsp + AVM_SAVEDTOPSP_OFFSET);
+    unsigned n = avm_total_actuals();
+
+
+	if(!p_topsp)
+		avm_log(ERROR,"'argument' called outside a function!\n");
+	else
+	if(n != 1)
+	{
+		avm_log(ERROR,"one argument (not %d) expected in 'argument'\n",n);
+	}
+	else
+	{
+		avm_memcell* arg = avm_getactual(0);
+
+		assert(arg->type == number_m && arg->data.numVal);
+		avm_mem_cell_clear(&retval);
+
+		arg = &stack[p_topsp + AVM_STACKENV_SIZE  + 1 + (unsigned)arg->data.numVal];
+
+		retval = *arg;
+	}
+}
+
+
+/* Arithmetic operations */
+void libfunc_sin(void)
+{
+	/* Basically we call sin which exist already...*/
+	unsigned n = avm_total_actuals();
+
+	if(n != 1)
+	{
+		avm_log(ERROR,"one argument (not %d) expected in 'sin'\n",n);
+	}
+	else
+	{
+		avm_mem_cell_clear(&retval);
+		avm_memcell* num = avm_getactual(0);
+
+		if(num->type != number_m)
+		{
+			avm_log(ERROR,"Argument of 'sin' was not a string\n");
+			return;
+		}
+
+		retval.type = number_m;
+		retval.data.numVal = sin(num->data.numVal);
+	}
+}
+
+
+void libfunc_cos(void)
+{
+	/* Basically we call cos which exist already...*/
+	unsigned n = avm_total_actuals();
+
+	if(n != 1)
+	{
+		avm_log(ERROR,"one argument (not %d) expected in 'cos'\n",n);
+	}
+	else
+	{
+		avm_mem_cell_clear(&retval);
+		avm_memcell* num = avm_getactual(0);
+
+		if(num->type != number_m)
+		{
+			avm_log(ERROR,"Argument of 'cos' was not a string\n");
+			return;
+		}
+
+		retval.type = number_m;
+		retval.data.numVal = cos(num->data.numVal);
+	}
+}
+
+void libfunc_sqrt(void)
+{
+	/* Basically we call sin which exist already...*/
+	unsigned n = avm_total_actuals();
+
+	if(n != 1)
+	{
+		avm_log(ERROR,"one argument (not %d) expected in 'sqrt'\n",n);
+	}
+	else
+	{
+		avm_mem_cell_clear(&retval);
+		avm_memcell* num = avm_getactual(0);
+
+		if(num->type != number_m)
+		{
+			avm_log(ERROR,"Argument of 'sqrt' was not a string\n");
+			return;
+		}
+
+		/* If the number is less than 0, return it*/
+		if(num->data.numVal < 0.0)
+		{
+			avm_log(WARNING,"'sqrt' called with negative argument %lf\n", num->data.numVal);
+			retval = *num;
+
+			return;
+		}
+
+		retval.type = number_m;
+		retval.data.numVal = sqrt(num->data.numVal);
+	}
+}
+
+
+/* String to num */
+void libfunc_strtonum(void)
+{
+	/* Basically we call strtod which exist already...*/
+	unsigned n = avm_total_actuals();
+
+	if(n != 1)
+	{
+		avm_log(ERROR,"one argument (not %d) expected in 'strtonum'\n",n);
+	}
+	else
+	{
+		avm_mem_cell_clear(&retval);
+		avm_memcell* str = avm_getactual(0);
+
+		char* endptr;
+
+		if(str->type != string_m)
+		{
+			avm_log(ERROR,"Argument of 'strtonum' was not a string\n");
+			return;
+		}
+
+		retval.type = number_m;
+		retval.data.numVal = strtod(str->data.strVal, &endptr);
+	}
+}
+
+/* Object related functions */
+void libfunc_objectmemberkeys(void)
+{
+
+}
+
+void  libfunc_objecttotalmembers(void)
+{
+
+}
+
+void libfunc_objectcopy(void)
+{
+
+
+}
+
+/* Input getter function */
+void libfunc_input(void)
+{
+
+
+}
