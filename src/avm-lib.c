e3476e09eab90ac88dcb4572b54ba873bd488005
gtheo <giorgostheodoraki@hotmail.com>
2024-06-06 00:32:21 +0300

    fixed argument

--- src/avm-lib.c
+++ src/avm-lib.c
@@ -92,7 +92,7 @@ static void libfunc_argument(void)
 	{
 		avm_memcell* arg = avm_getactual(0);
 
-		assert(arg->type == number_m && arg->data.numVal);
+		assert(arg->type == number_m);
 		avm_mem_cell_clear(&retval);
 
 		arg = &stack[p_topsp + AVM_STACKENV_SIZE  + 1 + (unsigned)arg->data.numVal];
